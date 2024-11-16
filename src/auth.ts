f2b9ab38cde80c1b751030c731f89f53a6bfec83
Kopymatic <kopymatic@gmail.com>
2024-11-15 22:51:33 -0700

    please fix my issue

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import Discord from '@auth/sveltekit/providers/discord';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
+	trustHost: true,
 	providers: [
 		Discord({
 			authorization: { params: { scope: 'identify guilds email' } }
