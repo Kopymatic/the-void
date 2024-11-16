22555579a809c36595a804d737c42f78afc11d8b
Kopymatic <kopymatic@gmail.com>
2024-11-15 22:31:05 -0700

    head improvements

--- src/routes/posts/[category]/[url]/+page.svelte
+++ src/routes/posts/[category]/[url]/+page.svelte
@@ -5,6 +5,12 @@
 	let { data }: { data: PageData } = $props();
 </script>
 
+<head>
+	<meta name="og:title" content={`${data.category}/${data.url}`} />
+	<meta name="og:site_name" content="the void" />
+	<meta name="og:description" content={data.description} />
+</head>
+
 <h2 class="mb-2">
 	{`[${data.id}]`}
 	<a href={`/posts/${data.category}/${data.url}`}>/posts/{data.category}/{data.url}</a>
