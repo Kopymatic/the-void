aa19ddb8dd51849eba37acd213513eef31104302
Kopymatic <kopymatic@gmail.com>
2024-11-23 20:31:09 -0700

    delete is working (i think)

--- src/components/modals/DeletePostModal.svelte
+++ src/components/modals/DeletePostModal.svelte
@@ -14,6 +14,7 @@
 		<h2 class="my-2">Are you sure you want to <b>permanently</b> delete this post?</h2>
 		This cannot be undone.
 		<hr />
+		<button class="bg-warn-red">Confirm Deletion</button>
 		<button type="button" onclick={() => (showModal = false)}>Cancel</button>
 	</form>
 </BaseModal>
--- src/routes/posts/[category]/[url]/+page.server.ts
+++ src/routes/posts/[category]/[url]/+page.server.ts
@@ -1,7 +1,8 @@
-import { error, type Actions } from '@sveltejs/kit';
+import { error, redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { prisma } from '$lib/server/database/database';
 import { env } from '$env/dynamic/private';
+import TodoistProvider from '@auth/sveltekit/providers/todoist';
 
 export const load: PageServerLoad = async ({ params }) => {
 	let category = params.category;
@@ -15,13 +16,32 @@ export const load: PageServerLoad = async ({ params }) => {
 
 	error(404, 'Not found');
 };
+
 export const actions = {
-	delete: async ({ request, locals, route }) => {
+	delete: async ({ request, locals, params }) => {
 		const session = await locals.auth();
 		if (!session || session.user?.id !== env.ADMIN_DISCORD_ID) {
 			error(401);
 		}
 
 		console.log('recieved delete request');
+
+		const category = params.category;
+		const url = params.url;
+
+		if (!category || !url) error(400, 'Invalid request');
+
+		const toDelete = await prisma.post.findFirst({ where: { category, url } });
+
+		if (!toDelete) error(404, 'Not found');
+
+		const deleted = await prisma.post
+			.delete({ where: { id: toDelete.id } })
+			.catch((e) => {
+				error(500, 'Database error');
+			})
+			.then(() => {
+				redirect(302, '/');
+			});
 	}
 } satisfies Actions;
