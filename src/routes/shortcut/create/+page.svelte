c46230d450e9f2d03377ccee46bd5e6c37c9133c
Kopymatic <git@kopymatic.com>
2025-06-02 22:23:32 -0600

    more tests on prod

--- src/routes/shortcut/create/+page.svelte
+++ src/routes/shortcut/create/+page.svelte
@@ -1,5 +1,12 @@
 <script>
 	import CreateShortcutModal from "$lib/components/modals/CreateShortcutModal.svelte";
+
+	import { page } from "$app/state";
+
+	let urlParams = page.url.searchParams.get("link");
+	let textParams = page.url.searchParams.get("text");
+	let titleParams = page.url.searchParams.get("title");
 </script>
 
+{`Link: ${urlParams} - Text: ${textParams} - Title: ${titleParams}`}
 <CreateShortcutModal showModal={true}></CreateShortcutModal>
