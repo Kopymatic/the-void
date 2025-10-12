86b48171ccce5347b544015cd1bfaa73b96596ff
Kopymatic <kopymatic@gmail.com>
2025-10-12 17:28:55 -0600

    slogan update

--- src/lib/homepageSlogans.ts
+++ src/lib/homepageSlogans.ts
@@ -1,32 +1,27 @@
 export const slogans = [
 	"my little nightmare",
-	"t h o u g h t s",
 	"a silly little place for a silly little person to share their silly little thoughts",
 	"you're cool",
 	"you're swagalicious",
-	"who shit myself",
 	"FunnymanV69 when ",
-	"who shit my pants",
 	"thanks Mom & Dad <3",
 	"Now with 0% AI!",
 	"No bad code here officer.",
 	"Debugging CSS hurts my entire being",
-	"does anyone read these? am i doing this all for nothing? why am i putting so much work into writing code for a site that nobody will ever see or care about, and then adding funny features that will only ever entertain me. Maybe there's something to be said about making things for the fun of it. Things that make US happy. I've always struggled with that. But even if nobody ever reads any of these, at least i had fun making it. That's really all that matters.",
 	"Made with Sveltekit <3",
 	"Made with TailwindCSS <3",
 	"Made with Prisma <3",
 	"Made with Love <3",
-	"Technoblade never dies!!!",
 	"hey future me let me know how Doom: The Dark Ages is",
 	"The light inside has broken but I still work",
-	"repost if you dip your cock in the Wendy's frosty",
 	"Millenials are killing the doorbell industry by texting 'here'",
 	"https://kopymatic.com/mike",
-	"im 19 years old and i've already wasted my entire life",
+	"im 20 years old and i've already wasted my entire life",
 	"auth sucks",
 	"hey past me Doom: the dark ages was pretty awesome",
 	"now with shortcuts",
-	"i have lost countless hours of my life to docker"
+	"i have lost countless hours of my life to docker",
+	"can someone explain six seven to me"
 ];
 
 export const getRandomSlogan = () => {
