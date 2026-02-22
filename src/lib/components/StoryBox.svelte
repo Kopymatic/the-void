b110c8dae44f26f545db125812455be968e38354
Kopymatic <kopymatic@gmail.com>
2026-02-22 08:52:23 -0700

    shortcuts now sorted, tweaks to storybox

--- src/lib/components/StoryBox.svelte
+++ src/lib/components/StoryBox.svelte
@@ -28,9 +28,9 @@
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <div
-	class={`border-primary ${holepunch ? "bg-primary-background" : "bg-secondary"} hover:border-accent relative rounded-lg border-2 p-4 transition-all duration-300 ease-in-out ${className} ${collapsed ? "line-clamp-1" : ""}`}
+	class={`border-primary ${holepunch ? "bg-primary-background" : "bg-secondary"} hover:border-accent relative rounded-lg border-2 px-4 pb-4 transition-all duration-300 ease-in-out ${className} ${collapsed ? "line-clamp-1" : ""}`}
 >
-	<div class="flex flex-row justify-between *:align-middle" onclick={collapseFunc}>
+	<div class="flex flex-row justify-between pt-4 pb-2 *:align-middle" onclick={collapseFunc}>
 		{#if collapsed}
 			<p class="text-2xl font-bold">{collapsedText ? collapsedText : titleText}</p>
 		{:else}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import Article from "$lib/components/Article.svelte";
 	import StoryBox from "$lib/components/StoryBox.svelte";
+	import { page } from "$app/state";
+	import type { SessionUser } from "$lib/server/types";
+	const user: SessionUser | undefined = $state(page.data.user);
+	const isAdmin = user?.isAdmin;
 </script>
 
 <div class="flex h-full flex-col">
@@ -12,20 +16,28 @@
 		<div
 			class="group mx-auto flex flex-col gap-2 px-4 *:box-border *:group-hover:scale-95 *:group-hover:opacity-75 *:hover:z-10 *:hover:scale-105 *:hover:opacity-100"
 		>
-			<StoryBox collapsedText="About me: ..." titleText="About me:">
+			<StoryBox collapsedText="About me: ..." titleText="About me:" startCollapsed={isAdmin}>
 				<p>
 					My name is Connor, or Kopymatic on the internet. I've been programming for about 5 years,
 					making a bunch of projects (which I'll cover in the following cards)
 				</p>
 			</StoryBox>
-			<StoryBox collapsedText="Languages: ..." titleText="I speak a bunch of languages!">
+			<StoryBox
+				collapsedText="Languages: ..."
+				titleText="I speak a bunch of languages!"
+				startCollapsed={isAdmin}
+			>
 				<p>
 					Well.. programming languages at least. I started out with Java, and quickly moved to
 					Kotlin and then kept learning from there. Currently I'm using in Typescript (and JS),
 					HTML, CSS, and I'm always trying to learn new languages.
 				</p>
 			</StoryBox>
-			<StoryBox collapsedText="Frameworks: ..." titleText="And Web Frameworks too">
+			<StoryBox
+				collapsedText="Frameworks: ..."
+				titleText="And Web Frameworks too"
+				startCollapsed={isAdmin}
+			>
 				<p>
 					I've dabbled in React and Vue, but I've really found enjoyment in <a
 						href="https://svelte.dev/">Svelte and SvelteKit</a
@@ -34,7 +46,11 @@
 				</p>
 			</StoryBox>
 
-			<StoryBox collapsedText="The Void: ..." titleText="The Void (you are here!)">
+			<StoryBox
+				collapsedText="The Void: ..."
+				titleText="The Void (you are here!)"
+				startCollapsed={isAdmin}
+			>
 				<p>
 					What started as a silly little blogging site made in SvelteKit, has since turned into my
 					main page. I've been working on it since November 2024 to make it my own toolkit. <a
@@ -42,7 +58,11 @@
 					>
 				</p>
 			</StoryBox>
-			<StoryBox collapsedText="kopy.fyi: ..." titleText="My link shortener, kopy.fyi">
+			<StoryBox
+				collapsedText="kopy.fyi: ..."
+				titleText="My link shortener, kopy.fyi"
+				startCollapsed={isAdmin}
+			>
 				<p>
 					Most recently, I made a personal link shortener, kopy.fyi. Currently its only for me to
 					use but having a way to make a shortened url that I can easily type in is amazing. Check
@@ -61,12 +81,12 @@
 			<StoryBox
 				collapsedText="Cool people: ..."
 				titleText="Some cool people/projects you should check out!"
+				startCollapsed={isAdmin}
 			>
 				<ul class="">
 					<li><a target="_blank" href="https://tboogle.com/">The Boogle Box (tboogle)</a></li>
 					<li><a target="_blank" href="https://alekeagle.com/">Alek Eagle (dad bot guy)</a></li>
 					<li><a target="_blank" href="https://cobalt.tools/">Cobalt (media downloader)</a></li>
-					<li>uh hey later me add some more cool people</li>
 				</ul>
 			</StoryBox>
 		</div>
