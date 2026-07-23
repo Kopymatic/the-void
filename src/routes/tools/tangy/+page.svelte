353d90cb506844a275c866040b80fe847e2668a4
Kopymatic <kopymatic@gmail.com>
2026-07-23 09:28:52 -0600

    begining of tangy

---
+++ src/routes/tools/tangy/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let face = $state("");
+	let tracesFound = $state(0);
+	let tracesRemoved = $state(0);
+</script>
+
+<div class="flex flex-col">
+	<label>
+		FACE <br />
+		<input type="text" placeholder="32/32" bind:value={face} />
+	</label>
+	<label>
+		Traces found <br />
+		<input type="number" placeholder="3" bind:value={tracesFound} />
+	</label>
+	<label>
+		Traces removed <br />
+		<input type="number" placeholder="2" bind:value={tracesRemoved} />
+	</label>
+</div>
+
+<h2>Output</h2>
+<div>
+	Started out by running hardware diagnostics, {face.replace("/", " of ")} tests passed.
+	<br />
+	We then ran antivirus scans, which {#if tracesFound === tracesRemoved}
+		found and removed {tracesRemoved} traces of viruses.
+	{:else}
+		found {tracesFound}, of which {tracesRemoved} were removed.
+	{/if}
+</div>
