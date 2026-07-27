e2aac658ca2136a6c201d499cc094a8969c6b356
Kopymatic <kopymatic@gmail.com>
2026-07-26 21:54:45 -0600

    dam

--- src/routes/tools/tangy/+page.svelte
+++ src/routes/tools/tangy/+page.svelte
@@ -1,24 +1,20 @@
 <script lang="ts">
+	/* eslint-disable svelte/no-useless-mustaches */
 	/* eslint-disable svelte/require-each-key */
 	let facePassed = $state(0);
 	let faceTotal = $state(undefined);
 	let tracesFound = $state(0);
 	let tracesRemoved = $state(undefined);
 	let updateSources = $state("");
+	let removedApps = $state("");
+	let removedAppsArr = $derived(removedApps.split(","));
+	let removedAppTotal = $derived(removedAppsArr.length);
+	let tempFiles = $state("");
 </script>
 
-<div class="flex flex-col">
-	<div class="flex">
-		<label>
-			FACE passed<br />
-			<input type="number" placeholder="e.g. 32" bind:value={facePassed} />
-		</label>
-		<label>
-			FACE total<br />
-			<input type="number" placeholder="e.g. 32" bind:value={faceTotal} />
-		</label>
-	</div>
-	<div class="flex">
+<div class="prose prose-invert flex flex-col">
+	<h3>Virus and PUPs</h3>
+	<div class="flex gap-2">
 		<label>
 			Traces found <br />
 			<input type="number" placeholder="e.g. 3" bind:value={tracesFound} />
@@ -28,6 +24,30 @@
 			<input type="number" placeholder="e.g. 2" bind:value={tracesRemoved} />
 		</label>
 	</div>
+	<label>
+		Removed apps? <br />
+		<input
+			type="text"
+			placeholder="Wave Browser, Shift Browser, Driver Support One, etc."
+			bind:value={removedApps}
+		/>
+	</label>
+
+	<h3>Diagnostics/Optimizations</h3>
+	<div class="flex gap-2">
+		<label>
+			FACE passed<br />
+			<input type="number" placeholder="e.g. 32" bind:value={facePassed} />
+		</label>
+		<label>
+			FACE total<br />
+			<input type="number" placeholder="e.g. 32" bind:value={faceTotal} />
+		</label>
+	</div>
+	<label>
+		Temp Files<br />
+		<input type="string" placeholder="e.g. 120MB" bind:value={tempFiles} />
+	</label>
 	<label>
 		Alternate Update Sources? <br />
 		<input
@@ -41,27 +61,42 @@
 <div class="prose prose-invert">
 	<h1>Output</h1>
 	<div>
-		Started out by running hardware diagnostics, {#if faceTotal}
-			with {facePassed} of our {faceTotal} tests passing.
-		{:else if facePassed}
-			with all {facePassed} {`test${facePassed > 1 ? "s" : ""}`} passing.
-		{:else}
-			with all tests passing.
-		{/if}
-		<br /><br />
-		We then ran antivirus scans which {#if !tracesFound}
+		Ran antivirus scans which {#if !tracesFound}
 			came back clean.
 		{:else if tracesFound === tracesRemoved || !tracesRemoved}
 			found and removed {tracesFound} {`trace${tracesFound > 1 ? "s" : ""}`} of malware.
 		{:else}
-			found {tracesFound}, of which {tracesRemoved} were removed.
+			found {tracesFound} traces, of which {tracesRemoved} were removed.
+		{/if}
+		{#if removedApps}
+			During the scans, we manually removed
+			{#each removedAppsArr as app, i}
+				{#if i + 1 === removedAppTotal && removedAppTotal >= 2}
+					{` and ${app}, `}
+				{:else}
+					{` ${app},`}
+				{/if}
+			{/each} as {removedAppTotal > 1 ? "these are" : "it is"} usually malicious or unwanted.
 		{/if}
 		<br /><br />
-		We ensured all available updates were run through Windows Update, the Microsoft Store,{#if updateSources}
+		Ran hardware diagnostics, {#if faceTotal}
+			with {facePassed} of our {faceTotal} tests passing.
+		{:else if facePassed}
+			with all {facePassed} of our {`test${facePassed > 1 ? "s" : ""}`} passing.
+		{:else}
+			with all tests passing.
+		{/if}
+		Also ran Windows optimizations, ensuring the system is running its best. Cleared any temporary files{#if tempFiles}
+			, freeing {tempFiles} of disk space.
+		{:else}
+			{" "} as well.
+		{/if}
+		<br /><br />
+		Ensured all available updates were run through Windows Update, the Microsoft Store,{#if updateSources}
 			{#each updateSources.split(",") as updateSource}
 				{" " + updateSource},
 			{/each}
-		{/if} and the Windows Package Manager.
+		{/if} and the Winget Package Manager.
 	</div>
 </div>
 
